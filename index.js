23d021ed9f0407090cbbd2821713c115500e058c
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2022-11-29 18:43:34 +0100

    complete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+var random = Math.floor(Math.random() * 10) + 1;
+let num3 = 32
+let num4 = 8
+let mod = num3 / num4
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 // Write your code here
